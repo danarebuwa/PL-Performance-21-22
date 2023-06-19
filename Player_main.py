0a552f6bce01d67fb8da822189435e4756be5791
danarebuwa <92400882+danarebuwa@users.noreply.github.com>
2023-06-19 13:58:33 +0100

    Update Player_main.py

--- Player_main.py
+++ Player_main.py
@@ -148,7 +148,7 @@ pl_df = pl_df.drop(columns=['Matches_x', 'Matches_y', 'Rk_y', 'Nation_y', 'Pos_y
 pl_df = pd.merge(pl_df, pl_poss, on='Player')
 pl_df = pl_df.drop(columns=['Matches','Att_x','TotDist_x','PrgDist_x','1/3_x','Prog_x'])
 pl_df = pd.merge(pl_df, pl_def, on='Player')
-pl_df = pl_df.drop(columns=['Matches_x', 'Matches_y', 'Rk_y', 'Nation_y', 'Pos_y', 'Squad_y', 'Age_y', 'Born_y', '90s_y','Rk_x', 'Nation_x', 'Pos_x', 'Squad_x', 'Age_x', 'Born_x', '90s_x', 'Gls.1','Ast.1','Ast_y', 'Ast_x','Matches','Matches_y','Matches_x','xA_x','xG.1','xA.1','Att_x','TotDist_x','PrgDist_x','xA_y','1/3_x','Prog_x'])
+pl_df = pl_df.drop(columns=['Rk_y', 'Nation_y', 'Pos_y', 'Squad_y', 'Age_y', 'Born_y', '90s_y','Rk_x', 'Nation_x', 'Pos_x', 'Squad_x', 'Age_x', 'Born_x', '90s_x', 'Ast_x','Matches'])
 pl_df = pd.merge(pl_df, pl_sca_gca, on='Player')
 pl_df = pl_df.drop(columns=['Matches_x', 'Matches_y', 'Rk_y', 'Nation_y', 'Pos_y', 'Squad_y', 'Age_y', 'Born_y', '90s_y','Rk_x', 'Nation_x', 'Pos_x', 'Squad_x', 'Age_x', 'Born_x', '90s_x', 'Gls.1','Ast.1','Ast_y', 'Ast_x','Matches','Matches_y','Matches_x','xA_x','xG.1','xA.1','Att_x','TotDist_x','PrgDist_x','xA_y','1/3_x','Prog_x'])
 #pl_df = pd.merge(pl_df, pl_shoot_df, on='Player')
